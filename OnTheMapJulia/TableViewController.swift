f8fd886fbed55bccab7f1e7b5e5e3119147c7f35
Julia Miller <julia.gao.miller@gmail.com>
2016-07-05 07:36:58 -0700

    tableview is now working!

---
+++ OnTheMapJulia/TableViewController.swift
@@ -0,0 +1,31 @@
+import Foundation
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    let appDelegate: AppDelegate = (UIApplication.sharedApplication().delegate as! AppDelegate)
+    var studentLocations = [[String:AnyObject]]()
+    
+    override func viewDidLoad() {
+        studentLocations = appDelegate.studentLocations
+        
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return studentLocations.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let singleStudent = studentLocations[indexPath.row]
+        
+        let first = singleStudent["firstName"] as! String
+        let last = singleStudent["lastName"] as! String
+        
+        print("24-tableviewcontroller: \(first) \(last)")
+        
+        let cell = self.tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
+        cell.textLabel?.text = "\(first) \(last)"
+        
+        return cell
+    }
+}
